988a52dde88e079d41c7a072fe3826a7e2896b83
unknown <bushrauroojansari@gmail.com>
2025-05-06 09:22:17 +0500

    voucher chag

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -630,7 +630,7 @@ class VoucherController extends Controller
         }
         
 
-        $data = $data->orderBy('document_date', 'desc')->orderBy('created_at', 'desc')->paginate(20);
+        $data = $data->orderBy('document_date', 'desc')->paginate(20);
 
         $parentUrl = request()->segments()[0];
 
