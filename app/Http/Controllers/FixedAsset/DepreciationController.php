8956c87add3beb8601f6657c1981dda8c5c20c70
BushraAnsari <bushrauroojansari@gmail.com>
2025-07-25 11:34:32 +0500

    code changes

--- app/Http/Controllers/FixedAsset/DepreciationController.php
+++ app/Http/Controllers/FixedAsset/DepreciationController.php
@@ -141,7 +141,7 @@ class DepreciationController extends Controller
                 $subAsset = FixedAssetSub::find((int)$sub_asset['sub_asset_id']);
                 if ($subAsset) {
                     $subAsset->total_depreciation += (float)$sub_asset['dep_amount'] ?? 0;
-                    $subAsset->current_value_after_dep = $subAsset->current_value_after_dep - (float)$sub_asset['dep_amount'];
+                    $subAsset->current_value_after_dep = (float)$sub_asset['after_dep_value']??0;
                     $subAsset->last_dep_date = Carbon::createFromFormat('d-m-Y', $sub_asset['to_date'])->addDay()->format('Y-m-d');
                     $subAsset->save();
                 }
