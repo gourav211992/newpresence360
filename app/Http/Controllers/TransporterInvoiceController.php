43a4298716c3e2a21e8bd6b41a13a63fd0ca6ed9
Gourav Kumar <sanamtaneja23@gmail.com>
2025-08-27 15:43:53 +0000

    code changes

--- app/Http/Controllers/TransporterInvoiceController.php
+++ app/Http/Controllers/TransporterInvoiceController.php
@@ -491,7 +491,7 @@ class TransporterInvoiceController extends Controller
             $customerPhoneNo = $request->customer_phone_no ?? null;
             $customerEmail = $request->customer_email ?? null;
             $customerGSTIN = $request->customer_gstin ?? null;
-            $customerName = $request->consignee_name ?? null;
+            // $customerName = $request->consignee_name ?? null;
             //If Customer is Regular, pick from Customer Master
             if ($customer->customer_type === ConstantHelper::REGULAR) {
                 $customerPhoneNo = $customer->mobile ?? null;
@@ -499,7 +499,7 @@ class TransporterInvoiceController extends Controller
                 $customerGSTIN = $customer->compliances?->gstin_no ?? null;
             } else {
                 //Check for customer details in Cash
-                if (!$customerPhoneNo || !$customerEmail || !$customerName) {
+                if (!$customerPhoneNo || !$customerEmail) {
                     DB::rollBack();
                     return response()->json([
                         'message' => 'Please enter all details of the customer',
