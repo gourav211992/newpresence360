856267f4f6a51b8999f1cbec79cbc8a74589d016
Gourav Kumar <sanamtaneja23@gmail.com>
2025-08-27 09:22:43 +0000

    code cahnges

--- app/Http/Controllers/PaymentVoucherController.php
+++ app/Http/Controllers/PaymentVoucherController.php
@@ -899,7 +899,7 @@ class PaymentVoucherController extends Controller
 
         } catch (Exception $e) {
             DB::rollBack();
-            // dd($e->getTraceAsString());
+            dd($e->getTraceAsString());
             return back()->withErrors('Something went wrong: ' . $e->getMessage());
         }
     }
